0bb009f818e2cc18c97d0d92f86f2d3d7085dce8
Artur Bak <abak@arturs-macbook-pro-2.home>
2023-08-28 13:13:10 +0200

    Added set split utils

--- opptimizer/putils.py
+++ opptimizer/putils.py
@@ -61,3 +61,90 @@ def overrides(interface_class):
 def oppdbg(object_to_write):
     if _PRINT_LOG_ENABLED:
         print(object_to_write)
+
+def set_move_chunk(chunk_ind_start, chunk_size, first, second):
+    
+    moved_count = 0
+    while moved_count < chunk_size and len(first) > chunk_ind_start:
+        el = first.pop(chunk_ind_start)
+        second.append(el)
+        moved_count += 1
+
+
+    return
+
+def set_get_index_pairs(index_list):
+
+    index_pairs_str = ''
+    ind_first = 0
+    ind_second = ind_first
+    list_len = len(index_list)
+
+    while ind_first < list_len:
+
+        add_pair_condiftion = False
+        
+        if ind_second >= list_len - 1:
+            add_pair_condiftion = True # we reach end of list
+        else:
+            #print(' i_s ' + str(index_list[ind_second]) + ' is+1:' + str(index_list[ind_second + 1]) + ' cond ' + str(index_list[ind_second + 1] > index_list[ind_second]))
+            if index_list[ind_second + 1] - index_list[ind_second] > 1 or index_list[ind_second + 1] < index_list[ind_second]:
+                add_pair_condiftion = True
+            
+
+        #add_pair_condiftion = index_list[ind_second] - index_list[ind_first] > 1 if ind_second < list_len else add_pair_condiftion
+
+        if add_pair_condiftion:
+            # ADD pair
+            if len(index_pairs_str) > 0:
+                index_pairs_str += ','
+            index_pairs_str += str(index_list[ind_first]) + ',' + str(index_list[ind_second])
+            ind_first = ind_second + 1
+            ind_second = ind_first
+        else:
+            ind_second += 1
+
+    return index_pairs_str
+
+def set_getSplitPairs(setToSplit, ratio, first_index, chunk_size, step):
+
+
+    curr_first = setToSplit
+    curr_second = []
+
+    print('getSplitPairs::LEN: ' + str(len(curr_first)) + ' -------->')
+
+    curr_ratio = 1.0
+
+    curr_ind = first_index
+    while curr_ratio > ratio:
+        set_move_chunk(curr_ind, chunk_size, curr_first, curr_second)
+        curr_first_size = len(curr_first)
+        curr_second_size = len(curr_second)
+        curr_ratio =  curr_first_size / (curr_second_size + curr_first_size)
+        curr_ind += step
+        print(str(curr_ind) + '('  + str(curr_ratio) + '):' + str(curr_first) + ' ' + str(curr_second))
+        if curr_ind >= len(curr_first):
+            curr_ind = 0
+
+    print('LEN_sum: ' + str(len(curr_first) + len(curr_second)))
+    return set_get_index_pairs(curr_first), set_get_index_pairs(curr_second)
+
+# Returns given text many times in string separated by comma 
+def set_getFilesInString(text, ntimes = 1):
+    str = ''
+    for i in range(ntimes):
+        if i>0:
+            str +=','
+        str += text
+    return str
+
+def set_getPairsAndFiles(pairs, file_name):
+    
+    pairs_count = len(pairs.split(','))
+    files = set_getFilesInString(file_name, int(pairs_count/2))
+
+    print(' == PAIRS: ' + str(pairs) + ' ' + str(files))
+    
+    return pairs, files
+
